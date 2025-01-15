efc80f0a8735ea1d55c1957aeb7a6b3b95803eca
ewanc <epclement@fortlewis.edu>
2025-01-15 14:37:56 -0700

    Added basic structure and styles

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to My First Web Project!");
+
+    const message = "Hello, Web Development!";
+    alert(message);
+});
